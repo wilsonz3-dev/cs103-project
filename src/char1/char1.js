64abc983783b1bf9aef2d48fe5454809ef051771
wilsonz3-dev <wilsonz3@bu.edu>
2025-04-15 23:00:57 -0400

    add randomized color gen + better color scheme

--- src/char1/char1.js
+++ src/char1/char1.js
@@ -3,3 +3,14 @@ function toggleSection(section) {
         section.hidden = !section.hidden;
     }
 }
+
+function colorAllHeadings() {
+    const headings = document.getElementsByTagName("h2");
+
+    const random_color_list = ["darkorange", "darkblue", "darkgreen", "crimson", "mediumvioletred"];
+
+    for (let i = 0; i < headings.length; i++) {
+        const randomIndex = Math.floor(Math.random() * random_color_list.length); // Generate a random integer (0-length of list)
+        headings[i].style.color = random_color_list[randomIndex]; // Set randomized color to the h2 tags
+    }
+}
