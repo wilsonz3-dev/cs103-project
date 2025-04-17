9018c5fee9df49f5f72cf5ddabc59bc5d1aa3fab
mcg0720 <mcg0720@bu.edu>
2025-04-17 12:24:22 -0400

    added javascript functioning

--- src/char3/char3.js
+++ src/char3/char3.js
@@ -1,1 +1,26 @@
-function fortune(){}
+let output = document.getElementById("output-fortune");
+
+function fortuneOne(){
+    document.getElementById("output-fortune").style.visibility="visible";
+    output.innerHTML = String("A dubious friend may be an enemy in camouflage.");
+}
+
+function fortuneTwo(){
+    document.getElementById("output-fortune").style.visibility="visible";
+    output.innerHTML = String("A fresh start will put you on your way.");
+}
+
+function fortuneThree(){
+    document.getElementById("output-fortune").style.visibility="visible";
+    output.innerHTML = String("Accept something that you cannot change, and you will feel better.");
+}
+
+function fortuneFour(){
+    document.getElementById("output-fortune").style.visibility="visible";
+    output.innerHTML = String("An acquaintance of the past will affect you in the near future.");
+}
+
+function fortuneFive(){
+    document.getElementById("output-fortune").style.visibility="visible";
+    output.innerHTML = String("Dedicate yourself with a calm mind to the task at hand.");
+}
